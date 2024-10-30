b40c90ff3e073e39886c98ca70a73c3ddd976f65
nguyentu111 <tunguyen118108@gmail.com>
2024-10-30 16:32:06 +0700

    u

--- app/routes/_client.tsx
+++ app/routes/_client.tsx
@@ -1,7 +1,7 @@
 import { LoaderFunctionArgs } from "@remix-run/node";
 import { json, Outlet } from "@remix-run/react";
-import { Footer } from "~/components/Footer";
-import { Header } from "~/components/Header";
+import { Footer } from "~/components/footer";
+import { Header } from "~/components/header";
 import { CartProvider } from "~/components/providers/cart-provider";
 import { ModalProvider } from "~/components/providers/modal-provider";
 import { prisma } from "~/lib/db.server";
